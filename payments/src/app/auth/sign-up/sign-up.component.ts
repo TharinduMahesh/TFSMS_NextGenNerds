65d800fd882b95e3940988cb1ade7c2af55b1ded
TharinduMahesh <155628660+TharinduMahesh@users.noreply.github.com>
2025-04-01 11:49:59 +0530

    updates

--- payments/src/app/auth/sign-up/sign-up.component.ts
+++ payments/src/app/auth/sign-up/sign-up.component.ts
@@ -1,34 +1,4 @@
-// import { Component } from '@angular/core';
-// import { CommonModule } from '@angular/common';
-// import { FormsModule } from '@angular/forms';
-// import { RouterModule } from '@angular/router';
 
-// @Component({
-//   selector: 'app-sign-up',
-//   standalone: true,
-//   imports: [CommonModule, FormsModule, RouterModule],
-//   templateUrl: './sign-up.component.html',
-//   styleUrls: ['./sign-up.component.css']
-// })
-// export class SignUpComponent {
-//   firstName: string = '';
-//   lastName: string = '';
-//   email: string = '';
-//   mobile: string = '';
-//   password: string = '';
-//   confirmPassword: string = '';
-//   role: string = '';
-
-//   onSubmit() {
-//     console.log('Sign up attempt', {
-//       firstName: this.firstName,
-//       lastName: this.lastName,
-//       email: this.email,
-//       mobile: this.mobile,
-//       role: this.role
-//     });
-//   }
-// }
 
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
---
+++ payments/src/app/models/user.model.ts
@@ -0,0 +1,10 @@
+export interface User {
+    id?: number;
+    Fname: string;
+    Lname: string;
+    email: string;
+    password: string;
+    confirmPassword: string;
+    phone: string;
+
+}

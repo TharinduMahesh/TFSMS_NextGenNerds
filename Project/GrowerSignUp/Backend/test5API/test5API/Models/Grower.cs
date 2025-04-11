e963d6ba1dc44c7d632c6a43b8c05f77427ebca5
nayodya <137150827+nayodya@users.noreply.github.com>
2025-04-11 16:14:14 +0530

    grower signup backend

---
+++ Project/GrowerSignUp/Backend/test5API/test5API/Models/Grower.cs
@@ -0,0 +1,9 @@
+﻿namespace test5API.Models
+{
+    public class Grower
+    {
+        public int GrowerId { get; set; }
+        public string GrowerEmail { get; set; }
+        public string GrowerPassword { get; set; }
+    }
+}

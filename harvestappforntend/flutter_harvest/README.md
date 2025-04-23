65877f182f7c12da5af2225dccd9c68ec7005d07
Nethmini-jayasekara <132090104+Nethmini-jayasekara@users.noreply.github.com>
2025-04-23 21:20:44 +0530

    Delete harvestappforntend/flutter_harvest directory

--- harvestappforntend/flutter_harvest/README.md
+++
@@ -1,3 +0,0 @@
-# flutter_harvest
-
-A new Flutter project.
--- harvestappforntend/flutter_harvest/lib/api_handler.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_harvest/harvest.dart';
-
-class ApiHandlers {
-  final String baseUri = "https://localhost:7160/api/harvest";
-
-  //Future<List<HarvestEntity> getUserData() async{
-   // return
-  //} 
-}

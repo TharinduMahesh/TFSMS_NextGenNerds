5dde2150ad8c1fb648659aa1ce083de9266f1341
TharinduMahesh <155628660+TharinduMahesh@users.noreply.github.com>
2025-04-24 16:52:14 +0530

    update homepage

--- payments/src/app/components/home/home.component.ts
+++ payments/src/app/components/home/home.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
+import {HeaderComponent} from '../header/header.component';
+import {FooterComponent} from '../footer/footer.component';
+
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  imports: [HeaderComponent, FooterComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
-     
+  scrollToContent(): void {
+    // Scroll down by one viewport height
+    window.scrollBy({
+      top: window.innerHeight, // Scroll down by one viewport height
+      behavior: 'smooth'
+    });
+  }
 }

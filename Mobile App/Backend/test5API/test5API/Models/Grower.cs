06cfcc09ea6a879a83467105cea4096f3508db29
nayodya <137150827+nayodya@users.noreply.github.com>
2025-04-28 23:50:39 +0530

    backend udpate

--- Mobile App/Backend/test5API/test5API/Models/Grower.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace test5API.Models
-{
-    public class Grower
-    {
-        public int GrowerId { get; set; }
-        public string GrowerEmail { get; set; }
-        public string GrowerPassword { get; set; }
-    }
-}
---
+++ Mobile App/Backend/test6API/test6API/Controllers/GrowerCreateAccountController.cs
@@ -0,0 +1,39 @@
+﻿// Controllers/GrowerSignUpController.cs
+using Microsoft.AspNetCore.Mvc;
+using test6API.Data;
+using test6API.Models;
+
+namespace test6API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class GrowerCreateAccountController : ControllerBase
+    {
+        private readonly ApplicationDbContext _context;
+
+        public GrowerCreateAccountController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> SignUp(GrowerCreateAccount growerAccount)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.GrowerCreateAccounts.Add(growerAccount);
+                await _context.SaveChangesAsync();
+                return Ok(new { message = "Grower create account successfully" });
+            }
+
+            return BadRequest(ModelState);
+        }
+
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            var growers = _context.GrowerCreateAccounts.ToList();
+            return Ok(growers);
+        }
+    }
+}

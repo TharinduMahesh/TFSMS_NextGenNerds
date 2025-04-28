d855d8414609bef2f31718a401c75087b3de0fc2
nayodya <137150827+nayodya@users.noreply.github.com>
2025-04-28 19:34:57 +0530

    connect grower signin, signup, create account

--- Mobile App/Project/GrowerOrder/FrontEnd/growertest/android/app/src/main/kotlin/com/example/growertest/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.growertest
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()
--- Mobile App/screens/tea_factory/android/app/src/main/kotlin/com/example/tea_factory/MainActivity.kt
+++
@@ -1,5 +0,0 @@
-package com.example.tea_factory
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity : FlutterActivity()

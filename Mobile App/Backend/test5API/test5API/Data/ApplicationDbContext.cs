d0035f683881e10a7a148a990b7d0de9c5feb3f2
nayodya <137150827+nayodya@users.noreply.github.com>
2025-04-28 19:42:30 +0530

    add back end signin signup

---
+++ Mobile App/Backend/test5API/test5API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using test5API.Models;
+
+namespace test5API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Grower> Growers { get; set; }
+    }
+}

e6a06301c15eed3e58cf54830854a01ac6b48c37
TharinduMahesh <155628660+TharinduMahesh@users.noreply.github.com>
2025-05-20 14:59:39 +0530

    update payament page

--- payments/src/app/app.component.ts
+++ payments/src/app/app.component.ts
@@ -1,16 +1,4 @@
-// import { Component } from '@angular/core';
-// import { RouterOutlet } from '@angular/router';
-// import {PaymentComponent} from './payment/payment.component';
 
-// @Component({
-//   selector: 'app-root',
-//   imports: [RouterOutlet,PaymentComponent],
-//   templateUrl: './app.component.html',
-//   styleUrl: './app.component.css'
-// })
-// export class AppComponent {
-//   title = 'payments';
-// }
 
 import { Component } from '@angular/core';
 import { RouterModule } from '@angular/router';

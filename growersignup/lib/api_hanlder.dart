50e1cc01442df85d4996de9a4b8bcdde3aa2f39f
nayodya <137150827+nayodya@users.noreply.github.com>
2025-07-17 20:18:18 +0530

    update frontend 7.3

---
+++ growersignup/lib/api_hanlder.dart
@@ -0,0 +1,35 @@
+import 'dart:convert';
+
+
+import 'package:growertest/grower_model.dart';
+import 'package:http/http.dart' as http;
+
+class ApiHandler {
+  Future<GrowerModel> getGrowerOrder(GrowerModel growerModel) async {
+    const url = 'https://localhost:7196/api/growerorders'; // Replace with your API URL
+
+    try{
+      final response = await http.post(
+        Uri.parse(url),
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: jsonEncode(growerModel.toJson()),
+      );
+      print('response status code: ${response.statusCode}');
+
+      if (response.statusCode == 200 || response.statusCode == 201) {
+        print('response body: ${response.body}');
+        GrowerModel newGrowerModel = GrowerModel.fromJson(jsonDecode(response.body));
+        return newGrowerModel;
+      } else {
+        print('response status code: ${response.statusCode}');
+        print('response body: ${response.body}');
+        throw Exception('Failed to load grower order');
+      }
+    } catch (e) {
+      print('Error: $e');
+      throw Exception('Failed to load grower order: $e');
+    }
+  }
+}

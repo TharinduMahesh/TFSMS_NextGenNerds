6b508fee94e3f47167439b71e61fb13e60a3646a
TharinduMahesh <155628660+TharinduMahesh@users.noreply.github.com>
2025-07-29 19:02:32 +0530

    add

--- Mobile App/Backend/test6API/test6API/obj/Debug/net8.0/test6API.AssemblyInfo.cs
+++ Mobile App/Backend/test6API/test6API/obj/Debug/net8.0/test6API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("test6API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e7a17c01b4259d5bc873f9ab619f135e44873aa9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6c48dce074950d19650978cb4148e9dc0f215fc4")]
 [assembly: System.Reflection.AssemblyProductAttribute("test6API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("test6API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
